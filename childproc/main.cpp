d804a3079999c9a2290b59a9a47c00a9ec2831e4
Dima <lopatalolol@gmail.com>
2019-06-27 12:40:35 +0300

    AHHA

--- childproc/main.cpp
+++ childproc/main.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
-#include <fstream>
-#include <unistd.h>
-#include <cstring>
 
 int main(){
 	std::cout << "Hello, I'm the child process\n";
-	char *a = new char[100];
-	std::strcpy(a, "Hello parent\n");
-	std::cout << &a << std::endl;
-	while(1);
 	return 0;
 }
